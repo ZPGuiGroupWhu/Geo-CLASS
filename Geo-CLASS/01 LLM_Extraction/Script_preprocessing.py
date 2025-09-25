62b3b97cf22a1e2a83ed14f43b6a65974783546c
Ankky-Zhao <68772050+Ankky-Zhao@users.noreply.github.com>
2025-09-25 15:09:36 +0200

    Update Script_preprocessing.py

--- Geo-CLASS/01 LLM_Extraction/Script_preprocessing.py
+++ Geo-CLASS/01 LLM_Extraction/Script_preprocessing.py
@@ -1,11 +1,11 @@
-# 脚本预处理：删除脚本中的注释代码，保留注释文本，减少token浪费，同时减少大模型的误解，提高大模型的准确性
+# Script preprocessing: Delete the comment code in the script, retain the comment text, reduce token waste, reduce misunderstanding of the large model, and improve the accuracy of the large model
 
 import os
 import re
 import time
 
 
-# 记录开始时间
+# Recording start time
 start_time = time.time()
 
 
@@ -25,7 +25,7 @@ def read_and_clean_file(file_path):
     comment_block = []
 
     for line in lines:
-        # 检查是否是特殊的单行注释
+        # Checks if it is a special single-line comment
         if line.strip().startswith('/****') and line.strip().endswith('****/'):
             new_lines.append(line)
             continue
@@ -34,7 +34,7 @@ def read_and_clean_file(file_path):
             comment_block = [line]
             if line.strip().endswith('*/'):
                 in_comment_block = False
-                # 检查注释代码块是否包含'https://'或'https://'，或是否符合is_code_block的条件
+                # Checks if the commented code block contains 'https://' or 'https://', ​​or meets the is_code_block condition
                 if any("https://" in l or "http://" in l for l in comment_block) or not any(
                         is_code_block(l) for l in comment_block):
                     new_lines.extend(comment_block)
@@ -45,7 +45,7 @@ def read_and_clean_file(file_path):
             comment_block.append(line)
             if line.strip().endswith('*/'):
                 in_comment_block = False
-                # 检查注释代码块是否包含'https://'或'https://'，或是否符合is_code_block的条件
+                # Checks if the commented code block contains 'https://' or 'https://', ​​or meets the is_code_block condition
                 if any("https://" in l or "http://" in l for l in comment_block) or not any(
                         is_code_block(l) for l in comment_block):
                     new_lines.extend(comment_block)
@@ -57,7 +57,7 @@ def read_and_clean_file(file_path):
                 new_lines.append(line)
             else:
                 new_lines.append(process_line(line))
-        # 您的生成路径
+        # Your build path
     result = '\n'.join(new_lines)
     return result
 
@@ -73,35 +73,35 @@ def process_line(line):
 
 
 def is_code_statement(text):
-    # 如果以, ; { } [ ] 其中之一结尾，则判定为代码行
+    # If it ends with one of , ; { } [ ], it is considered a code line
     if text.strip().endswith((',', ';', '{', '}', '[', ']')):
         return True
 
-    # 一级编程符号
+    # Level 1 programming symbols
     code_symbols_1 = ['=', '{', '}', '|', '&', ' //']
-    # JavaScript 关键字
+    # JavaScript Keywords
     js_keywords = ['var ', 'let', 'return', 'print', 'while', 'ee.', 'Map.', 'Export.']
-    # 检查符号和关键字
+    # Checking symbols and keywords
     if any(symbol in text for symbol in code_symbols_1) or any(keyword in text for keyword in js_keywords):
         return True
 
-    # 检查文本是否包含 'ing'
+    # Checks if text contains 'ing'
     if 'ing ' in text and '//' not in text:
         return False
 
-    # 检查文本是否以空格加大写字母开头
+    # Checks if text starts with a space and a capital letter
     if re.match(r'\s+[A-Z]', text):
         return False
 
-    # .后带单词字符(>=2个)组成的函数名后跟一对括号
+    # . followed by a function name consisting of a word character (>= 2) followed by a pair of brackets
     if re.search(r'\.\w{2,}\(', text):
         return True
 
-    # 新规则：如果 : 和 '' 同时出现，判定为代码
+    # New rule: If : and '' appear at the same time, it is considered a code
     if ':' in text and '\'' in text:
         return True
 
-    # 检查是否是以 // 或 # 或空格 开头且全是 // 或 # 或空格的行
+    # Check if the line starts with // or # or space and is entirely // or # or space
     if re.match(r'^(\/\/|#|\s)*$', text):
         return True
 
@@ -112,38 +112,38 @@ def is_code_statement(text):
 
 
 def is_code_block(text):
-    # 如果以{ } [ ] 其中之一结尾，则判定为代码行
+    # If it ends with one of { } [ ], it is considered a code line
     if text.strip().endswith(('{', '}', '[', ']')):
         return True
 
-    # 一级编程符号
+    # Level 1 programming symbols
     code_symbols_1 = ['=', '{', '}', '|', '&', ' //']
-    # JavaScript 关键字
+    # JavaScript Keywords
     js_keywords = ['var ', 'let', 'return', 'print', 'while', 'ee.', 'Map.', 'Export.']
-    # 检查符号和关键字
+    # Checking symbols and keywords
     if any(symbol in text for symbol in code_symbols_1) or any(keyword in text for keyword in js_keywords):
         return True
 
-    # 检查文本是否包含 'ing'
+    # Checks if text contains 'ing'
     if 'ing ' in text and '//' not in text:
         return False
 
-    # .后带单词字符(>=2个)组成的函数名后跟一对括号
+    # . followed by a function name consisting of a word character (>= 2) followed by a pair of brackets
     if re.search(r'\.\w{2,}\(', text):
         return True
 
     return False
 
 
-# # 替换为您的原文件目录路径
-# directory_path = 'F:\GEE\group3'
+# # Replace with your original file directory path
+# directory_path = 'Your_directory_path'
 # process_directory(directory_path)
 
-# # 记录结束时间
+# # Record end time
 # end_time = time.time()
 
-# # 计算代码的运行时间
+# # Calculate the running time of the code
 # execution_time = end_time - start_time
 
-# # 打印运行时间
-# print(f"代码执行时间为: {execution_time} 秒")
+# # Print running time
+# print(f"The code execution time is: {execution_time} 秒")
